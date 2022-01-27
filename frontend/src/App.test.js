bb412c5b4a3c8d08d7443434e7509e57f13f3767
Fred <mconyango@gmail.com>
2022-01-28 00:45:43 +0300

    Frontend Tests

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,17 @@
-import {render, screen} from '@testing-library/react';
+import React from 'react'
+import {render, fireEvent, waitFor, screen} from '@testing-library/react'
+import '@testing-library/jest-dom'
 import Form from "./Form";
+import Table from "./Table";
 
 test('Form is loading', () => {
     render(<Form/>);
     const formElement = screen.getByRole("form");
     expect(formElement).toBeInTheDocument();
-});
+})
+
+test('Form snapshot', () => {
+    const {asFragment} = render(<Form/>)
+
+    expect(asFragment(<Form/>)).toMatchSnapshot()
+})
